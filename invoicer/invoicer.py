c515399c72c25d031580c8506d7b91b5a7266e69
thermostatic <muxxeroerty@gmail.com>
2024-08-09 22:57:43 -0600

    Fixed Save button afirmo update

--- invoicer/invoicer.py
+++ invoicer/invoicer.py
@@ -5,11 +5,11 @@ from rich.logging import RichHandler
 import logging
 from playwright.sync_api import sync_playwright
 import pandas as pd
+from datetime import datetime, timedelta
 
 # Set up logging
 log_dir = pathlib.Path('logs')
-if not log_dir.exists():
-    log_dir.mkdir()
+log_dir.mkdir(exist_ok=True)
 log_file = log_dir / 'app.log'
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filename=log_file, filemode='w')
 logger = logging.getLogger(__name__)
@@ -19,6 +19,15 @@ logger.addHandler(handler)
 console = Console()
 
 def read_credentials(file_path):
+    """
+    Reads email and password from a specified file.
+
+    Args:
+        file_path (str): Path to the credentials file.
+
+    Returns:
+        tuple: A tuple containing email and password.
+    """
     email = None
     password = None
     with pathlib.Path(file_path).open('r') as f:
@@ -39,27 +48,24 @@ def read_credentials(file_path):
             raise ValueError(f"The file {file_path} is missing the 'Password: ' line. If the line exists, make sure you left a space after the :.")
 
 def read_data(file_path):
+    """
+    Reads data from a CSV file into a pandas DataFrame.
+
+    Args:
+        file_path (str): Path to the CSV file.
+
+    Returns:
+        pandas.DataFrame: DataFrame containing the data from the CSV file.
+    """
     try:
-        # Read the CSV file into a pandas DataFrame
         df = pd.read_csv(file_path)
-        
-        # Specify the columns to extract
         columns_to_extract = ['Owner Name', 'Invoiceable Hours', 'Invoice Rate', 'Green Waste Number', 'Invoice note']
-        
-        # Check if all columns exist in the DataFrame
         for column in columns_to_extract:
             if column not in df.columns:
                 raise ValueError(f"The column '{column}' does not exist in the CSV file.")
-        
-        # Reorder the columns to put Owner Name first
         df = df[columns_to_extract]
-        
-        # Replace NaN values with None
         df = df.where(pd.notnull(df), '')
-        
-        # Return the extracted DataFrame
         return df
-        
     except FileNotFoundError:
         raise ValueError(f"The file {file_path} does not exist.")
     except pd.errors.EmptyDataError:
@@ -70,27 +76,53 @@ def read_data(file_path):
         raise ValueError(f"An unexpected error occurred while reading the file {file_path}: {e}")
 
 def initialize_invoicer(email, password, p):
+    """
+    Initializes the invoicer by logging into the application using Playwright.
+
+    Args:
+        email (str): User's email.
+        password (str): User's password.
+        p (Playwright): Playwright instance.
+
+    Returns:
+        tuple: Browser and Page instances.
+    """
     try:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto("https://app.afirmo.com/")
         page.wait_for_timeout(5000)      
-        # Interact with login form
         page.locator("#username").fill(email)
         page.locator("#password").fill(password)
-        
         page.get_by_role("button", name="Sign in").click()
-
         return browser, page
     except Exception as e:
         logger.error(f"Error initializing invoicer: {e}")
         raise
 
 def create_invoice(owner_name, invoiceable_hrs, hourly_rate, green_waste_no, invoice_note, page):
+    """
+    Creates an invoice for the given owner details.
+
+    Args:
+        owner_name (str): Name of the invoice owner.
+        invoiceable_hrs (float): Number of invoiceable hours.
+        hourly_rate (float): Hourly rate for the invoice.
+        green_waste_no (int): Number of green waste units.
+        invoice_note (str): Note for the invoice.
+        page (Page): Playwright Page instance.
+    """
     page.goto("https://app.afirmo.com/sales/invoices/add")
     page.wait_for_timeout(1000)
     page.get_by_placeholder("Search contacts").fill(owner_name)
     page.get_by_role("button", name=owner_name).click()
+    
+    date_obj = datetime.strptime(page.locator("#date").input_value(), '%d/%m/%Y')
+    two_weeks_later = date_obj + timedelta(days=14)
+    new_date_str = two_weeks_later.strftime('%d/%m/%Y')
+    
+    page.locator("#expiryDate").fill('')
+    page.locator("#expiryDate").fill(str(new_date_str))
     page.get_by_placeholder("Select product").click()
     page.get_by_role("button", name="Gardening- hourly").click()
     page.locator("#items\[0\]\.qty").fill('')
@@ -98,19 +130,19 @@ def create_invoice(owner_name, invoiceable_hrs, hourly_rate, green_waste_no, inv
     page.locator("#items\[0\]\.unitAmount").fill('')
     page.locator("#items\[0\]\.unitAmount").fill(str(hourly_rate))   
     
-    if green_waste_no is not None and green_waste_no != '' and green_waste_no != 0:
+    if green_waste_no and green_waste_no != '' and green_waste_no != 0:
         page.get_by_role("button", name="new row").click()
         page.get_by_placeholder("Select product").click()
         page.get_by_role("button", name="Green waste removal").click()
         page.locator("#items\[1\]\.qty").fill('')
         page.locator("#items\[1\]\.qty").fill(str(green_waste_no))
     
-    if invoice_note is not None and invoice_note != '':
+    if invoice_note:
         page.get_by_role("button", name="new Notes").click()
         page.locator("input[name=\"inotes\\[0\\]\\.text\"]").fill(str(invoice_note))
     
     page.wait_for_timeout(1000)
-    page.query_selector('xpath=/html/body/div[1]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div/form/div[1]/div[2]/button').click()
+    page.get_by_role("button", name="Save").click()
     page.wait_for_timeout(5000)
     
 def main():
@@ -148,7 +180,6 @@ def main():
                     console.print(f"Error creating invoice for {owner_name}", style="red")
                     failed_owners.append(owner_name)
                     
-                    # Close the current browser session and set to None to reinitialize on next iteration
                     if browser:
                         browser.close()
                     browser = None
@@ -156,7 +187,6 @@ def main():
 
                 progress.update(task, advance=1)
 
-            # Close the browser if it's still open
             if browser:
                 browser.close()
 
